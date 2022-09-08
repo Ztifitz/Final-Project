877eed230e4b5e4ec42659777b17dbade86cb6d0
Ztifitz <101848882+Ztifitz@users.noreply.github.com>
2022-09-08 18:30:47 -0500

    Update charts.js

--- Dashboard/charts.js
+++ Dashboard/charts.js
@@ -2,7 +2,7 @@ function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
   
-    // Use the list of sample names to populate the select options
+    // Use the list of model names to populate the select options
     d3.json("AAPL.json").then((data) => {
       var modelNames = data.names;
   
@@ -13,7 +13,7 @@ function init() {
           .property("value", name);
       });
   
-      // Use the first sample from the list to build the initial plots
+      // Use the lack of model name from the list to build the initial plots
       var firstModel = modelNames[0];
       buildCharts(firstModel);
       buildMetadata(firstModel);
@@ -36,25 +36,14 @@ function init() {
       console.log("data",data)
       var modeldesc = data.desc;
       console.log(modeldesc)
-      // Filter the data for the object with the desired sample number
-      //var resultmd = modeldesc.filter(modelObj => modelObj.key == model);
-      //var resultArray = modeldesc.model;
-      //console.log(resultArray)
-      //var md = resultmd[0];
+     
       // Use d3 to select the panel with id of `#sample-metadata`
       var PANEL = d3.select("#sample-metadata");
   
       // Use `.html("") to clear any existing metadata
       PANEL.html("");
   
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
       
-      //var resultmod = modeldesc.filter(modelObj => modelObj.key == model)
-      //var resmod = resultmod[0]
-      //console.log(resultmod)
-      //console.log(resmod)
       var moddesc = modeldesc[model]
       console.log(moddesc)
       //Object.entries(moddesc).filter(([key, value]) => {
@@ -66,29 +55,16 @@ function init() {
     };
   
   
-// DELIVERABLE 1 Requirements
-// Create a Horizontal Bar Chart
+// Create a Line Chart
 
 // 1. Create the buildCharts function.
 function buildCharts(model) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("AAPL.json").then((data) => {
     
-    //var dict = {};
-    //var addPair = function (myKey, myValue) {
-    //dict[myKey] = myValue;
-    //};
-    //var giveValue = function (myKey) {
-    //return dict[myKey];
-  //};
     console.log(model)
     var Dates = Object.values(data.Date);
-
-
-    //var HistDate = [];
-    //Date.forEach(key => {key < 50  });
-    //HistDate.append(element)
-    
+        
     //console.log(Dates)
 
     var AdjClose = Object.values(data.Adjusted_Close);
@@ -98,31 +74,6 @@ function buildCharts(model) {
     var Modeldata = data[model];
     var Modelvalues = Object.values(Modeldata)
     //console.log(Modelvalues)
-
-    //Object.entries(data.Date).filter(key => k > 50);//[data.Date[0,40]]
-    //console.log(Date)
-    // 3. Create a variable that holds the samples array. 
-    //var samples = data.samples;
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    //var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    //  5. Create a variable that holds the first sample in the array.
-    //var result = resultArray[0];
-
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    //var  ids = result.otu_ids;
-    //var labels = result.otu_labels.slice(0, 10).reverse();
-    //var values = result.sample_values.slice(0,10).reverse();
-
-    //var bubbleLabels = result.otu_labels;
-    //var bubbleValues = result.sample_values;
-
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
-
-    //var yticks = ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
-
-    //console.log(yticks)
 
     // 8. Create the trace for the bar chart. 
     var lineDataAC = {
